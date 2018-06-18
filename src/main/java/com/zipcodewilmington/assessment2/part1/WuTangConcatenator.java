91302ee147034783325db0b3165926be47924500
Tommy Rogers <tommyr@TommyRogers.local>
2018-06-18 10:30:30 -0400

    finished parts 1 and 2

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,4 +1,23 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+
+    int input;
+
+    public WuTangConcatenator(int input){
+      this.input = input;
+    }
+
+    public Boolean isTang(){
+        return (input%5 == 0);
+    }
+
+    public Boolean isWu(){
+        return(input%3 == 0);
+
+    }
+
+    public Boolean isWuTang(){
+        return(input%15 == 0);
+    }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,4 +1,72 @@
 package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
+
+
+    public Integer [] merge(Integer [] firstArray, Integer [] secondArray){
+        Integer [] mergedArray = new Integer[(firstArray.length) + (secondArray.length)];
+        for(int x =0; x < firstArray.length; x++){
+            mergedArray[x] = firstArray[x];
+        }
+        int j= 0;
+        for(int y = firstArray.length; y < mergedArray.length; y++){
+            mergedArray[y] = secondArray[j];
+            j++;
+        }
+        return mergedArray;
+    }
+
+    public Integer [] rotate(Integer [] input, int numberOfRotations){
+        Integer [] rotatedArray = new Integer[input.length];
+            for(int x = 0; x < numberOfRotations; x++){
+                rotatedArray[rotatedArray.length-numberOfRotations+x] = input[x];
+            }
+            int j = 0;
+            for(int x = numberOfRotations; x < rotatedArray.length; x++){
+                rotatedArray[j] = input[x];
+                j++;
+        }
+        return rotatedArray;
+    }
+
+
+
+    public Integer countOccurrance(Integer[] array1,Integer[] array2, int valueToEvaluate){
+        int count = 0;
+        for(int element : array1){
+            if(element == valueToEvaluate)
+                count++;
+        }
+        for(int element : array2){
+            if(element == valueToEvaluate)
+                count++;
+        }
+
+        return count;
+    }
+
+    public Integer mostCommon(Integer[] array){
+        int finalCounter = 0;
+        int tempCounter;
+        int answer = 0;
+
+        for(int element: array){
+            tempCounter = countOccurrance(array, element);
+            if(tempCounter > finalCounter){
+                finalCounter = tempCounter;
+                answer = element;
+            }
+        }
+        return answer;
+    }
+
+    public Integer countOccurrance(Integer[] array, int valueToEvaluate){
+        int count = 0;
+        for(int element : array){
+            if(element == valueToEvaluate)
+                count++;
+        }
+        return count;
+    }
+
 }
